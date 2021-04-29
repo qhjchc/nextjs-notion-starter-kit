6d6ccde3d5c017647755746687ec4ea416f8939d
Children <children19930928@gmail.com>
2021-04-29 22:54:05 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'e8a23d749e0a4a5e8842a213932b3776',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'QHJ',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Chiu HJ',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'socialImageTitle',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'qhjchc',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
